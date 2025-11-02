85072aecd219febff8fe1b0309508f30b03992b0
MANJEET SINGH <manjeetsinghyk@gmail.com>
2025-11-02 12:29:28 +0530

    Update googleAuth.js
    updated the google auth code for production env

--- server/lib/googleAuth.js
+++ server/lib/googleAuth.js
@@ -2,8 +2,12 @@ import { Google } from "arctic";
 import dotenv from "dotenv";
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const google = new Google(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
-  `http://localhost:8080/api/auth/google/callback`
+  isProduction
+    ? "https://authify-server-c1zn.onrender.com/api/auth/google/callback" // deployed backend
+    : "http://localhost:8080/api/auth/google/callback" // local dev
 );
