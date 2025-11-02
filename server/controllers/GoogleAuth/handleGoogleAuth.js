535247a4ee68c99ab8c00de43b09f37d2cb58f3c
manjeetsi52 <manjeetsinghyk@gmail.om>
2025-11-02 20:11:08 +0530

    updated cookies because of 401 error

--- server/controllers/GoogleAuth/handleGoogleAuth.js
+++ server/controllers/GoogleAuth/handleGoogleAuth.js
@@ -1,6 +1,12 @@
 import { decodeIdToken, generateCodeVerifier, generateState } from "arctic";
 import { google } from "../../lib/googleAuth.js";
-import { createUserWithOauth, generateAccessToken, generateRefreshToken, getUserWithOauthId, linkUserWithOauth } from "../../services/service.js";
+import {
+  createUserWithOauth,
+  generateAccessToken,
+  generateRefreshToken,
+  getUserWithOauthId,
+  linkUserWithOauth,
+} from "../../services/service.js";
 import { Session } from "../../models/session.js";
 
 export const getGoogleLoginPage = (req, res) => {
@@ -47,63 +53,73 @@ export const getGoogleLoginCallback = async (req, res) => {
         .status(400)
         .json({ error: "Invalid login attempt , please try again!" });
     }
-    let token = await google.validateAuthorizationCode(code,codeVerifier)
-    if(!token) return res.status(400).json({error:'Invalid login attempt, Please try again!'})
-     
-    const claims = decodeIdToken(token.idToken())
-    const {sub:googleUserId,name,email,picture} = claims;
-    
-    let user = await getUserWithOauthId({googleUserId,email})
+    let token = await google.validateAuthorizationCode(code, codeVerifier);
+    if (!token)
+      return res
+        .status(400)
+        .json({ error: "Invalid login attempt, Please try again!" });
+
+    const claims = decodeIdToken(token.idToken());
+    const { sub: googleUserId, name, email, picture } = claims;
+
+    let user = await getUserWithOauthId({ googleUserId, email });
     // console.log('user',user)
-    
+
     //user not exist
-    if(!user){
+    if (!user) {
       user = await createUserWithOauth({
         name,
         email,
-        password:null,
-        provider:'google',
-        providerAccountId:googleUserId,
-        picture
-      })
+        password: null,
+        provider: "google",
+        providerAccountId: googleUserId,
+        picture,
+      });
     }
-    
-    //user exist but not linked with oauth 
-    if(user && !user.providerAccountId)
-    {
+
+    //user exist but not linked with oauth
+    if (user && !user.providerAccountId) {
       await linkUserWithOauth({
-        userId:user._id,
-        provider:'google',
-        providerAccountId:googleUserId
-      })
+        userId: user._id,
+        provider: "google",
+        providerAccountId: googleUserId,
+      });
     }
 
     const tempSession = new Session({
-        userId:user._id,
-        userAgent: req.headers['user-agent'],
-        ip:req.clientIp,
-        expiresAt: new Date(Date.now() + 7*24*60*60*1000)
-    })
+      userId: user._id,
+      userAgent: req.headers["user-agent"],
+      ip: req.clientIp,
+      expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    });
 
-    const refreshToken = generateRefreshToken({sessionId: tempSession.id})
-     tempSession.refreshToken = refreshToken
-     const session = await tempSession.save()
+    const refreshToken = generateRefreshToken({ sessionId: tempSession.id });
+    tempSession.refreshToken = refreshToken;
+    const session = await tempSession.save();
 
-     const accessToken = generateAccessToken({
-      id:user._id,
-      name:user.name,
-      email:user.email,
-      sessionId: session._id
-     })
+    const accessToken = generateAccessToken({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      sessionId: session._id,
+    });
 
-     const baseConfig = {httpOnly:true,sameSite:"none"}
+    const baseConfig = { httpOnly: true, secure: true, sameSite: "none" };
 
-     res.cookie('access_token',accessToken,{...baseConfig,maxAge: 15*60*1000})
-     res.cookie('refresh_token',refreshToken,{...baseConfig,maxAge:7*24*60*60*1000})
+    res.cookie("access_token", accessToken, {
+      ...baseConfig,
+      maxAge: 15 * 60 * 1000,
+    });
+    res.cookie("refresh_token", refreshToken, {
+      ...baseConfig,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
 
-    return res.redirect(`https://authify-client-19tc.onrender.com/auth-success`)//for hosting
+    return res.redirect(
+      `https://authify-client-19tc.onrender.com/auth-success`
+    ); //for hosting
     // return res.redirect(`http://localhost:5173/auth-success`)//for development
   } catch (error) {
-    return res.status(500).json({error:error.message})
+    return res.status(500).json({ error: error.message });
   }
 };
