5086c1a9190df2d22597569ecadaaeb14228e4bc
MANJEET SINGH <manjeetsinghyk@gmail.com>
2025-11-02 10:42:47 +0530

    Update apiBaseUrl.js
    added render backend link

--- client/src/utils/apiBaseUrl.js
+++ client/src/utils/apiBaseUrl.js
@@ -4,7 +4,7 @@
   //   ? "http://localhost:8080"          //  local backend
   //   : "https://820f2fdfcab2.ngrok-free.app";      //  Ngrok URL
 
-    const API_BASE_URL = "http://localhost:8080" 
-    // const API_BASE_URL = "https://2991a29fed99.ngrok-free.app" 
+    // const API_BASE_URL = "http://localhost:8080" 
+    const API_BASE_URL = "https://authify-server-c1zn.onrender.com" 
     
 export default API_BASE_URL;
