4358054725157fa522d4c999db3bd1f3d0bfa2fa
manjeetsi52 <manjeetsinghyk@gmail.om>
2026-01-09 18:21:59 +0530

    client deplolyed on vercel

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ app.use(
       "http://localhost:5173",
       "https://f98163cba279.ngrok-free.app",
       "https://authify-client-19tc.onrender.com",
+      "https://authify-fawn.vercel.app/"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "HEAD"],
