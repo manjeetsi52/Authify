e39778c459e226c24d22d9192b0da750512eafd0
manjeetsi52 <manjeetsinghyk@gmail.om>
2026-01-09 21:28:50 +0530

    oauth optimized

--- client/src/pages/GoogleAuth/GoogleAuth.jsx
+++ client/src/pages/GoogleAuth/GoogleAuth.jsx
@@ -1,37 +1,35 @@
-import axios from "axios"
-import { useEffect } from "react"
-import './GoogleAuth.css'
-import API_BASE_URL from "../../utils/apiBaseUrl"
-export const GoogleAuth = ()=>{
+import axios from "axios";
+import { useEffect } from "react";
+import "./GoogleAuth.css";
+import API_BASE_URL from "../../utils/apiBaseUrl";
 
-        useEffect(()=>{
-            let timer = setTimeout(() => {
-                handleRedirection()
-            }, 1000);
+export const GoogleAuth = () => {
+  useEffect(() => {
+    const redirectToGoogle = async () => {
+      try {
+        const res = await axios.get(`${API_BASE_URL}/google`, {
+          withCredentials: true,
+        });
 
-            return ()=>clearTimeout(timer)
-        },[])
-
-        const handleRedirection = async()=>{
-                try {
-                    const res= await axios.get(`${API_BASE_URL}/google`,{withCredentials:true})
-                    if(res.status === 200)
-                    {
-                        const {url} = res.data
-                        window.location.href = url//navigation to google auth url
-                    }else{
-                        console.log('unexpected response',res)
-                    }
-                } catch (error) {
-                    console.log('error from GoogleAuth Page',error.message)
-                }
+        if (res.status === 200 && res.data?.url) {
+          window.location.href = res.data.url;
+        } else {
+          console.error("Invalid response from server", res);
         }
+      } catch (error) {
+        console.error(
+          "Google Auth redirect failed:",
+          error.response?.data || error.message
+        );
+      }
+    };
 
-    return (
-        <>
-            <div className="redirection">
-                <h1>Redirecting...</h1>
-            </div>
-        </>
-    )
-}
+    redirectToGoogle();
+  }, []);
+
+  return (
+    <div className="redirection">
+      <h1>Redirecting to Google...</h1>
+    </div>
+  );
+};
