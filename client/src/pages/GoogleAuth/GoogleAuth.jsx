70b206ccd56f7bd24cb480ba303a8589d6781981
manjeetsi52 <manjeetsinghyk@gmail.om>
2026-01-10 10:07:15 +0530

    testing oauth client

--- client/src/pages/GoogleAuth/GoogleAuth.jsx
+++ client/src/pages/GoogleAuth/GoogleAuth.jsx
@@ -2,34 +2,36 @@ import axios from "axios";
 import { useEffect } from "react";
 import "./GoogleAuth.css";
 import API_BASE_URL from "../../utils/apiBaseUrl";
-
 export const GoogleAuth = () => {
   useEffect(() => {
-    const redirectToGoogle = async () => {
-      try {
-        const res = await axios.get(`${API_BASE_URL}/google`, {
-          withCredentials: true,
-        });
+    let timer = setTimeout(() => {
+      handleRedirection();
+    }, 1000);
 
-        if (res.status === 200 && res.data?.url) {
-          window.location.href = res.data.url;
-        } else {
-          console.error("Invalid response from server", res);
-        }
-      } catch (error) {
-        console.error(
-          "Google Auth redirect failed:",
-          error.response?.data || error.message
-        );
-      }
-    };
-
-    redirectToGoogle();
+    return () => clearTimeout(timer);
   }, []);
 
+  const handleRedirection = async () => {
+    try {
+      const res = await axios.get(`${API_BASE_URL}/google`, {
+        withCredentials: true,
+      });
+      if (res.status === 200) {
+        const { url } = res.data;
+        window.location.href = url; //navigation to google auth url
+      } else {
+        console.log("unexpected response", res);
+      }
+    } catch (error) {
+      console.log("error from GoogleAuth Page", error.message);
+    }
+  };
+
   return (
-    <div className="redirection">
-      <h1>Redirecting to Google...</h1>
-    </div>
+    <>
+      <div className="redirection">
+        <h1>Redirecting...</h1>
+      </div>
+    </>
   );
 };
